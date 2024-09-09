d144e3d521772916657d1fbb8025f2445aa05aed
dodo240703 <a20202396@pucp.edu.pe>
2024-09-08 23:46:18 -0500

    avance html
    avance de las vistas de dashboard y de la tabla de de fechas de arribo

--- src/main/java/com/app/trado/controller/AdminZonalController.java
+++ src/main/java/com/app/trado/controller/AdminZonalController.java
@@ -2,11 +2,24 @@ package com.app.trado.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/adminzonal")
 public class AdminZonalController {
-    @GetMapping("/adminzonal")
+    @GetMapping("/vacio")
     public String home() {
         return "AdminZonal/starter-AdminZonal";
     }
+
+    @GetMapping("/dashboard")
+    public String showDashboard() {
+        return "AdminZonal/dashboard-AdminZonal";
+    }
+
+    @GetMapping("/fechasArribo")
+    public String showFechasArribo() {
+        return "AdminZonal/tablaFechaArribo-AdminZonal";
+    }
+
 }
