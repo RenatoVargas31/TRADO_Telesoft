58ec42829756e422e2dd26778f9fcfd326c3ed55
RenatoVargas31 <rlvs_312001_1999@outlook.com>
2024-09-09 13:17:32 -0500

    Cambios menores

--- src/main/java/com/app/trado/controller/SuperAdminController.java
+++ src/main/java/com/app/trado/controller/SuperAdminController.java
@@ -2,15 +2,16 @@ package com.app.trado.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/superadmin")
 public class SuperAdminController {
-    @GetMapping("/superadmin")
+    @GetMapping("/vacio")
     public String home() {
         return "SuperAdmin/starter-SuperAdmin";
     }
 
     @GetMapping("/tabla")
-    public String tabla() {
-        return "SuperAdmin/prueba";}
+    public String tabla() {return "SuperAdmin/prueba";}
 }
