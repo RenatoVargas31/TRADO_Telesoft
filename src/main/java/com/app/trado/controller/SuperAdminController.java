a3ded9462a950867cb748bc3a1cc0886ecfd740e
RenatoVargas31 <rlvs_312001_1999@outlook.com>
2024-09-10 18:44:36 -0500

    Error de scroll - solucionado Linkeo de vista de ayuda en Superadmin

--- src/main/java/com/app/trado/controller/SuperAdminController.java
+++ src/main/java/com/app/trado/controller/SuperAdminController.java
@@ -8,9 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/superadmin")
 public class SuperAdminController {
+    @GetMapping("/incio")
+    public String viewInicio() {
+        return "SuperAdmin/prueba";
+    }
 
     @GetMapping("/dashboard")
     public String viewDashboard() {
         return "SuperAdmin/dashboard-SAdmin";
+    }
+    @GetMapping("/ayuda")
+    public String viewAyuda() {
+        return "SuperAdmin/ayuda-SAdmin";
     }
 }
